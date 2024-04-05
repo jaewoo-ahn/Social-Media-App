9780b304af3afdd0f895174ad09aef23e70a909f
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-04-05 21:49:32 +0900

    redux

---
+++ client/src/scenes/index.js
@@ -0,0 +1,4 @@
+export { default as HomePage } from "./homePage";
+export { default as LoginPage } from "./loginPage";
+export { default as Navbar } from "./navbar";
+export { default as ProfilePage } from "./profilePage";
